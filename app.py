c0caa2581f55a819c3a3ee4559cd3d76b622fad0
IanTaylor <76837421+iTaylorCodes@users.noreply.github.com>
2021-06-09 23:56:33 -0700

    Added new user functionality

--- app.py
+++ app.py
@@ -1,12 +1,44 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "secret"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
 
 connect_db(app)
 db.create_all()
+
+@app.route('/')
+def list_users():
+    """Shows a list of all users in db"""
+    users = User.query.all()
+    return render_template('list.html', users=users)
+
+@app.route('/new_user_form')
+def show_new_user_form():
+    """Shows a form to add a new user"""
+    return render_template('user_form.html')
+
+@app.route('/new_user_form', methods=['POST'])
+def create_user():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    image_url = request.form['image_url']
+
+    new_user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(f"/{new_user.id}")
+
+@app.route('/<int:user_id>')
+def show_user(user_id):
+    """Show details about a user"""
+    user = User.query.get_or_404(user_id)
+    return render_template('details.html', user=user)
--- models.py
+++ models.py
@@ -1,1 +1,34 @@
 """Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+# MODELS GO BELOW!
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    def __repr__(self):
+        usr = self
+        return f"<User id={usr.id} first_name={usr.first_name} last_name={usr.last_name} image_url={usr.image_url}>"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+
+    first_name = db.Column(db.String(50),
+                           nullable=False,
+                           unique=True)
+
+    last_name = db.Column(db.String(50),
+                          nullable=False)
+
+    image_url = db.Column(db.String,
+                          nullable=False,
+                          default='https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg')
+
+    
